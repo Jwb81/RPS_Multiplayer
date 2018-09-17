2b8561eb2132b8663220b4e1d1a36ba48f1d89ee
Jason Barnett <jayb21@neo.rr.com>
2018-09-17 01:16:02 -0400

    two players can now connect to the game and listen for actions from each other

--- logic.js
+++ logic.js
@@ -9,7 +9,15 @@ var config = {
 };
 firebase.initializeApp(config);
 
+// contains the firebase database
 var database = firebase.database();
+
+// contains the game object reference string
+var currentGame;
+
+// contains each player's randomly generated key
+var me;
+var opponent;
 
 // holds all of the games so users can connect to it via the game key
 var gamesRef = database.ref();
@@ -65,24 +73,131 @@ var connectedRef = database.ref(".info/connected");
 //     console.log(snap.val());
 // })
 
+var setGameListener = function() {
+    database.ref(currentGame).on('value', function (snap) {
+        let info = snap.val();
+        console.log(info);
+
+        // get the opponents reference ID
+        if (!opponent) {
+            if (info.creator != me && info.joiner == me) {
+                opponent = info.creator;
+                setOpponentListener(); // create new listeners with the new opponent variable
+            } else if (info.creator == me && info.joiner != me && info.joiner != undefined) {
+                opponent = info.joiner;
+                setOpponentListener(); // create new listeners with the new opponent variable
+            }
+        }
+    })
+}
+
+var setMeListener = function() {
+    database.ref(currentGame + '/' + me).on('value', function (snap) {
+        console.log(currentGame + '/' + me);
+        console.log(snap.val());
+    })
+}
+
+var setOpponentListener = function() {
+    database.ref(currentGame + '/' + opponent).on('value', function (snap) {
+        console.log(currentGame + '/' + opponent);
+        console.log(snap.val());
+    })
+}
+
+
 
 
 $('#join-game').on('click', function () {
     var input = $('#game-key').val();
 
-    if (!input) 
+    if (!input)
         return;
 
     // var gamesRef = database.ref();
-    console.log(input);
-    
+    // console.log(input);
+
+    var playerObj = {
+        guess: "",
+        losses: 0,
+        ties: 0,
+        wins: 0
+    }
+
     gamesRef.once('value', function (snap) {
-        if (snap.hasChild(input)) {
-            database.ref(input + '/users/user2').set(input);
-            alert('the child exists');
+        var info = snap.val();
+
+        if (snap.child(input).player2 != undefined) {
+            console.log("There are already two players... You cannot join");
+            return;
         }
-        else
+        if (snap.hasChild(input)) {
+            // console.log(snap.val()[input].creator);
+
+            database.ref(input)
+                .push(playerObj)
+                .then((snapshot) => {
+                    var key = snapshot.key;
+                    database.ref(input + '/joiner').set(key);
+                    me = key;
+
+                    opponent = info[input].creator;
+                    currentGame = input;
+                    setMeListener();
+                    setOpponentListener();
+                    setGameListener();
+                })
+            // database.ref(input + '/player2').set(input);
+
+            // alert('the child exists');
+        } else
             alert('DNE');
+
+        
     })
 
 });
+
+$('#create-game').on('click', function () {
+    var input = $('#game-key').val();
+
+    if (!input)
+        return;
+
+    gamesRef.once('value', function (snap) {
+        if (snap.hasChild(input)) {
+            alert('Game id already exists');
+        } else {
+            // create new game and add all variables here
+            // database.ref(input + '/users/user2').set(input);
+            // alert('DNE: creating new game');
+            var gameObj = {
+                messages: [],
+                creator: null,
+                joiner: null,
+            }
+
+            var playerObj = {
+                guess: "",
+                losses: 0,
+                ties: 0,
+                wins: 0
+            }
+
+            database.ref(input).set(gameObj);
+            database.ref(input)
+                .push(playerObj)
+                .then((snap) => {
+                    var key = snap.key;
+                    database.ref(input + '/creator').set(key);
+                    me = key;
+                    currentGame = input;
+                    setMeListener();
+                    setGameListener();
+                })
+
+        }
+
+    })
+
+})
